b0bd70c7e7a3bf229e1826cfbe2635c2e6ae43ae
YanLuo_glb <YanLuo_glb@git.com>
2022-03-19 19:52:00 +0800

    更新路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,75 @@
 // 引入VueRouter
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";
 
 // 引入路由组件
-import Home from '../pages/Home'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import TaskList from '../pages/TaskList'
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import TaskList from "../pages/TaskList";
+import MyItem from "../pages/MyItem";
+import AddItem from "../pages/AddItem";
+import AllItem from "../pages/MyItem/AllItem";
+import OnSell from "../pages/MyItem/OnSell";
+import Others from "../pages/MyItem/Others";
 
- const router = new VueRouter({
-    routes:[
-        // 主页组件
-        {
-            name:"Home",
-            path:'/home',
-            component:Home,
-            // Home的子组件
-            children:[
-                
-            ]
-        },
-        // 登录组件
-        {
-            name:"Login",
-            path:'/login',
-            component:Login,
-        },
-        //注册组件 
-        {
-            name:"Register",
-            path:'/register',
-            component:Register,
-        },
-        //任务列表
-        {
-            name:"TaskList",
-            path:"/taskList",
-            component:TaskList
-        }
-    ]
-})
+const router = new VueRouter({
+  routes: [
+    // 主页组件
+    {
+      name: "Home",
+      path: "/home",
+      component: Home,
+      // Home的子组件
+      children: [],
+    },
+    // 登录组件
+    {
+      name: "Login",
+      path: "/login",
+      component: Login,
+    },
+    //注册组件
+    {
+      name: "Register",
+      path: "/register",
+      component: Register,
+    },
+    //任务列表
+    {
+      name: "TaskList",
+      path: "/taskList",
+      component: TaskList,
+    },
+    //添加商品
+    {
+      name: "AddItem",
+      path: "/addItem",
+      component: AddItem,
+    },
+    //我的商品
+    {
+        name: "MyItem",
+        path: "/myItem",
+        component: MyItem,
+        children:[
+          {
+            name:"AllItem",
+            path: "/myItem/allItem",
+            component: AllItem,
+          },
+          {
+            name:"OnSell",
+            path: "/myItem/onSell",
+            component: OnSell,
+          },
+          {
+            name:"Others",
+            path: "/myItem/others",
+            component: Others,
+          },
+        ]
+      },
+  ],
+});
 
-export default router
+export default router;
