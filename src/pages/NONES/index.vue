da96dd4c94bd3cd6510dca995ae5e98b7544bd6d
Yanluo <Yanluo_glb>
2022-05-16 15:51:04 +0800

    页面跳转使用的空白页

---
+++ src/pages/NONES/index.vue
@@ -0,0 +1,23 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "NONES",
+  mounted(){
+    //   挂载的时候检测本地是否有注册的信息
+    let userInfor = this.$store.state.userInfor
+    if(userInfor.registerUserInfo = {} || userInfor.userName===""){
+        // 没有注册过，以及没有登录过
+        this.$router.push("/login")
+    }else{
+        this.$router.push("/home")
+    }
+    
+  }
+}
+</script>
+
+<style>
+</style>
